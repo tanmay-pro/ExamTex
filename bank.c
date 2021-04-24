36a4e7f3d654c22c0dbc42f19785c55f17cb6d7a
Laksh Balani <lakshbalani2002@gmail.com>
2021-04-24 21:55:00 +0530

    changed laksh branch

--- bank.c
+++ bank.c
@@ -134,3 +134,27 @@ mcq insert_question(stack s1,char pre[],char post[],double post_num,char post_li
 
     return question;
 }
+
+void question_bank( )
+{
+    char x, y, z;
+    x = getchar();
+    char pre[10], post[10];
+    double post_num;
+    char post_line[100];
+    struct stack s1;
+    int i=0;
+    s1.top = -1;
+    mcq* arr[10];
+    mcq questions[10];
+    while (x!=EOF)
+    {
+        questions[i]=insert_question(s1,pre,post,post_num,post_line);
+        arr[i]=&questions[i];
+        //printf("%d %lf\n",i,arr[i]->difficulty);
+        i++;
+        x=getchar();
+    }
+    for(i=0;i<4;i++)
+     printf("%d %s\n",i,arr[i]->options[3]);
+}
