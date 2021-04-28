4e1a75d7aac83306bc9112ef796bd7aaf1e2b2bd
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-04-28 16:21:31 +0530

    Introduced New Types And Made Connection

--- Question_Bank/bank.c
+++ Question_Bank/bank.c
@@ -31,25 +31,20 @@ void pop(stack *ps)
     }
 }
 
-//bool isEmpty(stack *ps)
-//{
-//    return (ps->top == -1) ? true : false;
-//} // Function never used anywhere
-
 double input_difficulty(stack s1, char *pre)
 {
     char y, z;
     double post_num;
-    y = getchar();
+    y = (char)getchar();
     while (y != '{')
     {
-        y = getchar();
+        y = (char)getchar();
     }
     push(&s1, y);
     scanf("%[^=]s", pre);
     getchar();
     scanf("%lf", &post_num);
-    z = getchar();
+    z = (char)getchar();
     if (z == '}')
     {
         pop(&s1);
@@ -60,18 +55,18 @@ double input_difficulty(stack s1, char *pre)
 char *input_text(stack s1, char *pre, char *post_line, char *buffer)
 {
     char y, z;
-    y = getchar();
+    y = (char)getchar();
     while (y != '{')
     {
-        y = getchar();
+        y = (char)getchar();
     }
     push(&s1, y);
     scanf("%[^=]s", pre);
     getchar();
-    post_line[0] = getchar();
+    post_line[0] = (char)getchar();
     while (post_line[0] == ' ')
     {
-        post_line[0] = getchar();
+        post_line[0] = (char)getchar();
     }
     post_line[1] = '\0';
     scanf("%[^}]s", buffer);
@@ -96,20 +91,20 @@ mcq *insert_mcq(stack s1)
     question->difficulty = input_difficulty(s1, pre);
     strcpy(question->text, input_text(s1, pre, post_line, buffer));
 
-    y = getchar();
+    y = (char)getchar();
     while (y != '{')
     {
-        y = getchar();
+        y = (char)getchar();
     }
     push(&s1, y);
     scanf("%[^=]s", pre);
     getchar();
     for (int i = 0; i < 3; i++)
     {
-        post_options[i][0] = getchar();
+        post_options[i][0] = (char)getchar();
         while (post_options[i][0] == ' ')
         {
-            post_options[i][0] = getchar();
+            post_options[i][0] = (char)getchar();
         }
         post_options[i][1] = '\0';
         scanf("%[^,]s", buffer);
@@ -117,10 +112,10 @@ mcq *insert_mcq(stack s1)
         getchar();
     }
 
-    post_options[3][0] = getchar();
+    post_options[3][0] = (char)getchar();
     while (post_options[3][0] == ' ')
     {
-        post_options[3][0] = getchar();
+        post_options[3][0] = (char)getchar();
     }
     post_options[3][1] = '\0';
     scanf("%[^}]s", buffer);
@@ -134,23 +129,23 @@ mcq *insert_mcq(stack s1)
     {
         strcpy(question->options[i], post_options[i]);
     }
-    y = getchar();
+    y = (char)getchar();
     while (y != '{')
     {
-        y = getchar();
+        y = (char)getchar();
     }
     push(&s1, y);
     scanf("%[^=]s", pre);
     getchar();
-    post_correct[0] = getchar();
+    post_correct[0] = (char)getchar();
     while (post_correct[0] == ' ')
     {
-        post_correct[0] = getchar();
+        post_correct[0] = (char)getchar();
     }
     post_correct[1] = '\0';
     scanf("%[^}]s", buffer);
     strcat(post_correct, buffer);
-    z = getchar();
+    z = (char)getchar();
     if (z == '}')
     {
         pop(&s1);
@@ -168,23 +163,23 @@ fill_up *insert_fill_up(stack s1)
     char buffer[1000];
     question->difficulty = input_difficulty(s1, pre);
     strcpy(question->text, input_text(s1, pre, post_line, buffer));
-    y = getchar();
+    y = (char)getchar();
     while (y != '{')
     {
-        y = getchar();
+        y = (char)getchar();
     }
     push(&s1, y);
     scanf("%[^=]s", pre);
     getchar();
-    post_correct[0] = getchar();
+    post_correct[0] = (char)getchar();
     while (post_correct[0] == ' ')
     {
-        post_correct[0] = getchar();
+        post_correct[0] = (char)getchar();
     }
     post_correct[1] = '\0';
     scanf("%[^}]s", buffer);
     strcat(post_correct, buffer);
-    z = getchar();
+    z = (char)getchar();
     if (z == '}')
     {
         pop(&s1);
@@ -195,7 +190,7 @@ fill_up *insert_fill_up(stack s1)
 void question_bank()
 {
     char x, y, z;
-    x = getchar();
+    x = (char)getchar();
     char pre[10], post[10];
     struct stack s1;
     int i, mcq_index = 0, fill_up_index = 0;
@@ -203,16 +198,16 @@ void question_bank()
 
     while (x != EOF)
     {
-        y = getchar();
+        y = (char)getchar();
         while (y != '{')
         {
-            y = getchar();
+            y = (char)getchar();
         }
         push(&s1, y);
         scanf("%[^=]s", pre);
         getchar();
         scanf("%[^}]s", post);
-        z = getchar();
+        z = (char)getchar();
         if (z == '}')
         {
             pop(&s1);
@@ -227,7 +222,7 @@ void question_bank()
             fill_up_arr[fill_up_index] = insert_fill_up(s1);
             fill_up_index++;
         }
-        x = getchar();
+        x = (char)getchar();
     }
     // Debugging start
 //    for (i = 0; i < 4; i++)
