41038db01d47b1c0363bef09c8a0f809334a8846
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-05-09 16:51:47 +0530

    Comments

--- Question_Bank/delete_questions.c
+++ Question_Bank/delete_questions.c
@@ -5,8 +5,8 @@ void functionToDeleteQuestionBank(ptrnode qb[])
 	int input_id = 0;
 	printf("Please Enter the id of the Question bank you want to Delete");
 	br;
-	scanf("%d", &input_id);
+	scanf("%d", &input_id); // Obtains input id
 	ptrnode temp = qb[input_id];
-	free_bank(temp);
-	qb[input_id] = NULL;
+	free_bank(temp); //Frees Memory
+	qb[input_id] = NULL; // Makes the array element NULL to indicate deletion of question bank
 }
